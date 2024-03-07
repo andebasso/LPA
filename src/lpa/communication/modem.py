014b7f6e44802be016029bbe9324006fa80fbd6a
Anderson Basso de Oliveira <andebasso@yahoo.com.br>
2024-03-07 10:51:28 -0300

    modified:   README.md 	docs/SGP.21-v3.0.pdf 	docs/SGP.22-v3.0-1.pdf 	src/__init__.py 	src/communication/ 	src/interfaces/ 	src/lpa/lds/ 	src/lpa/lpd/ 	src/lpa/lpr/ 	src/lpa/lui/ 	src/utils/

--- src/lpa/communication/modem.py
+++ src/lpa/communication/modem.py
@@ -1,27 +1,8 @@
-import serial
-import time
+from .at_commands import RESET, ECHO_OFF  # Importa comandos AT necessários
+from .utilities import send_at_command  # Supondo que esta é uma função de utilidade que você tem
 
-class Modem:
-    def __init__(self, port='/dev/ttyUSB2', baudrate=115200):
-        self.port = port
-        self.baudrate = baudrate
-        self.connection = None
+def reset_modem():
+    send_at_command(RESET)
 
-    def open_connection(self):
-        self.connection = serial.Serial(self.port, self.baudrate, timeout=1)
-        # Pode adicionar mais configurações de inicialização aqui
-
-    def close_connection(self):
-        if self.connection:
-            self.connection.close()
-            self.connection = None
-
-    def send_at_command(self, command):
-        if not self.connection:
-            raise Exception('Modem not connected')
-        self.connection.write((command + '\r\n').encode())
-        time.sleep(1)  # Aguardar resposta
-        response = self.connection.read_all().decode()
-        return response
-
-    # Aqui você pode adicionar mais métodos específicos para seu modem
+def disable_echo():
+    send_at_command(ECHO_OFF)
--- src/lpa/communication/smdp.py
+++ src/lpa/communication/smdp.py
@@ -1,13 +1,13 @@
 import requests
 
-class SMDPClient:
-    def __init__(self, smdp_address, smdp_credentials):
-        self.smdp_address = smdp_address
-        self.smdp_credentials = smdp_credentials
+def get_esim_profile(smdp_address, activation_code):
+    """Solicita um perfil eSIM do servidor SM-DP+."""
+    headers = {'Content-Type': 'application/json'}
+    payload = {'activationCode': activation_code}
+    
+    response = requests.post(f'{smdp_address}/profiles', headers=headers, json=payload)
+    if response.status_code == 200:
+        return response.json()  # Retorna o perfil eSIM
+    else:
+        response.raise_for_status()  # Lança uma exceção se ocorreu um erro
 
-    def download_profile(self, iccid):
-        # Aqui você implementaria a lógica para comunicar-se com a SM-DP+ e baixar um perfil
-        # usando as credenciais fornecidas
-        pass
-
-    # Adicione mais métodos conforme necessário para outras interações SM-DP+
